1284dbf51f7b63b117e55aacf60991a1530e678b
hugosenari <hugosenari@gmail.com>
2022-03-12 02:55:50 -0300

    chore: re index gerbil

---
+++ master/flake.nix
@@ -0,0 +1,90 @@
+{
+  description = ''A dynamic website generator'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-gerbil-master.flake = false;
+  inputs.src-gerbil-master.owner = "jasonprogrammer";
+  inputs.src-gerbil-master.ref   = "refs/heads/master";
+  inputs.src-gerbil-master.repo  = "gerbil";
+  inputs.src-gerbil-master.type  = "github";
+  
+  inputs."argparse".dir   = "nimpkgs/a/argparse";
+  inputs."argparse".owner = "riinr";
+  inputs."argparse".ref   = "flake-pinning";
+  inputs."argparse".repo  = "flake-nimble";
+  inputs."argparse".type  = "github";
+  inputs."argparse".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."argparse".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."cligen".dir   = "nimpkgs/c/cligen";
+  inputs."cligen".owner = "riinr";
+  inputs."cligen".ref   = "flake-pinning";
+  inputs."cligen".repo  = "flake-nimble";
+  inputs."cligen".type  = "github";
+  inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."jester".dir   = "nimpkgs/j/jester";
+  inputs."jester".owner = "riinr";
+  inputs."jester".ref   = "flake-pinning";
+  inputs."jester".repo  = "flake-nimble";
+  inputs."jester".type  = "github";
+  inputs."jester".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jester".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."markdown".dir   = "nimpkgs/m/markdown";
+  inputs."markdown".owner = "riinr";
+  inputs."markdown".ref   = "flake-pinning";
+  inputs."markdown".repo  = "flake-nimble";
+  inputs."markdown".type  = "github";
+  inputs."markdown".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."markdown".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."mustache".dir   = "nimpkgs/m/mustache";
+  inputs."mustache".owner = "riinr";
+  inputs."mustache".ref   = "flake-pinning";
+  inputs."mustache".repo  = "flake-nimble";
+  inputs."mustache".type  = "github";
+  inputs."mustache".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mustache".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimcrypto".dir   = "nimpkgs/n/nimcrypto";
+  inputs."nimcrypto".owner = "riinr";
+  inputs."nimcrypto".ref   = "flake-pinning";
+  inputs."nimcrypto".repo  = "flake-nimble";
+  inputs."nimcrypto".type  = "github";
+  inputs."nimcrypto".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimcrypto".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."regex".dir   = "nimpkgs/r/regex";
+  inputs."regex".owner = "riinr";
+  inputs."regex".ref   = "flake-pinning";
+  inputs."regex".repo  = "flake-nimble";
+  inputs."regex".type  = "github";
+  inputs."regex".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."regex".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."uuids".dir   = "nimpkgs/u/uuids";
+  inputs."uuids".owner = "riinr";
+  inputs."uuids".ref   = "flake-pinning";
+  inputs."uuids".repo  = "flake-nimble";
+  inputs."uuids".type  = "github";
+  inputs."uuids".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."uuids".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-gerbil-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-gerbil-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
